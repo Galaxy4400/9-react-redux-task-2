9ad0d6f1bab94a5a630228492651327aae21efed
Moiseev Evgeny <moiseevEO@yandex.ru>
2024-08-09 10:57:59 +0300

    commit

--- src/actions/update-task.js
+++ src/actions/update-task.js
@@ -1,9 +1,13 @@
 import { updateTask as updateTaskApi } from "../api/tasks-api";
 import { getTasks } from "./get-tasks";
+import { setUpdatingStatus } from "./set-updating-status";
 
 export const updateTask = (taskId, updatingData) => {
 	return (dispatch) => {
+		dispatch(setUpdatingStatus(true));
+
 		updateTaskApi(taskId, updatingData)
-			.then(() => dispatch(getTasks()));
+			.then(() => dispatch(getTasks()))
+			.finally(() => dispatch(setUpdatingStatus(false)));
 	};
 };
